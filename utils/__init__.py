f51845f6513da67be8329ace9df5e7a05fe13687
yushaoqi <1603453211@qq.com>
2022-05-10 21:14:21 +0800

    fix bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,6 @@
 # @Time   : 2021/12/14 22:06
 # @Author : 余少琪
 
-from utils.otherUtils.InstallUtils.install_requirements import InstallRequirements
-
-InstallRequirements().text_comparison()
+# from utils.otherUtils.InstallUtils.install_requirements import InstallRequirements
+#
+# InstallRequirements().text_comparison()

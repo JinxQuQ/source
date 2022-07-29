30b4764e13953befde4bdf08812f1631465c7d93
余少琪 <1603453211@qq.com>
2022-07-29 18:03:21 +0800

    1、fix bugs

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -126,7 +126,7 @@ def pytest_terminal_summary(terminalreporter):
     _SKIPPED = len([i for i in terminalreporter.stats.get('skipped', []) if i.when != 'teardown'])
     _TOTAL = terminalreporter._numcollected
     _TIMES = time.time() - terminalreporter._sessionstarttime
-
+    INFO.logger.error(f"用例总数: {_TOTAL}")
     INFO.logger.error(f"异常用例数: {_ERROR}")
     ERROR.logger.error(f"失败用例数: {_FAILED}")
     WARNING.logger.warning(f"跳过用例数: {_SKIPPED}")
@@ -134,6 +134,6 @@ def pytest_terminal_summary(terminalreporter):
 
     try:
         _RATE = _PASSED / _TOTAL * 100
-        logger.info("用例成功率: %.2f" % _RATE + " %")
+        INFO.logger.info("用例成功率: %.2f" % _RATE + " %")
     except ZeroDivisionError:
-        logger.info("用例成功率: 0.00 %")
+        INFO.logger.info("用例成功率: 0.00 %")
